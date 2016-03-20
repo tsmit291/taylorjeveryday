e291b2d453adb5c74a1e30c2188cbfd2121a02a7
= <tsmit291@icloud.com>
2016-03-19 20:47:18 -0600

    edited bio

--- js/app.js
+++ js/app.js
@@ -19,4 +19,4 @@ app.config(function($routeProvider){
   })
 });
 
-console.log("I'm an extremely passionate and hard worker. I have been known to Rick Roll my coworkers and provide a copious amount of corny puns when the going gets tough. I write everything down in my notebook, so if you ever forget what you said, come find me as I most likely have what you need. I do things the right way, 100% of the time, even if it isn't the easiest way. I back up, check up and follow up to ensure careless mistakes are kept to an absolute minimum.");
+console.log("I'm an extremely passionate and hard worker. I have been known to Rick Roll my coworkers and provide a copious amount of corny puns when the going gets tough. I write everything down in my notebook, so if you ever forget what you said, come find me as I most likely have what you need. I do things the right way, 100% of the time, even if it isn't the easiest way. I back up, check up and follow up to ensure careless mistakes are kept to an absolute minimum. Most of all, I love to learn. I'm looking for longevity with a company that will provide me mentorship and opportunities to continue learning new things.");
